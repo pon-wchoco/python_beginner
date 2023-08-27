291972857da73ffe131447c33c7215b1f759391e
sat304 <sat304@gmail.com>
2023-08-27 15:33:28 +0900

    型

--- src/py010/py010-1.py
+++ src/py010/py010-1.py
@@ -11,14 +11,19 @@ def main():
     hairetsu_gata = [1, 2, 3]
 
     # 辞書型
-    jisho_gata = {"key1": "value1", "key2": "value2", "key3": "value3"}
+    jisho_gata = {
+        "key1": "value1",
+        "key2": "value2",
+        "key3": "value3",
+        "key4": "value4",
+    }
 
     # ブール型(True か False だけ)
     bool_gata = True
     bool_gata = False
 
     # タプル型
-    tuple_gata = ("value1", "value2")
+    tuple_gata = ("value1", "value2", "value3", "value4")
 
 
 if __name__ == "__main__":
