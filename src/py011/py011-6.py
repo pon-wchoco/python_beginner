c21f6c5e7486614f52b4b43d774387fc6ec41cd5
sat304 <sat304@gmail.com>
2023-09-09 15:41:58 +0900

    スコープ追加

---
+++ src/py011/py011-6.py
@@ -0,0 +1,31 @@
+# ループや分岐はスコープにならない！
+def main():
+    hensu_main = "メイン変数"
+
+    # 関数
+    def kansu():
+        hensu_main = "メイン（関数の内側）"
+        print(hensu_main)
+
+    # ループ
+    for i in range(3):
+        hensu_loop = "ループ"
+        hensu_main = "メイン（ループの内側）"
+        print(str(i) + ":" + hensu_loop + "," + hensu_main)
+
+    # 分岐
+    if True:
+        hensu_bunki = "分岐"
+        hensu_main = "メイン（分岐の内側）"
+        print(hensu_bunki + "," + hensu_main)
+
+    kansu()
+
+    print(hensu_main)
+    # 以下はスコープ外のように見えるが、使えてしまう
+    print(hensu_loop)
+    print(hensu_bunki)
+
+
+if __name__ == "__main__":
+    main()
